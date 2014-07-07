797e96dd15ec8c0686ed40e39625b8e5351f6ff9
kadhikari <krishna.adhikari@canaltp.fr>
2014-07-07 17:19:41 +0200

    chaos: A validator to call jormungandr added

---
+++ chaos/validator.py
@@ -0,0 +1,63 @@
+# Copyright (c) 2001-2014, Canal TP and/or its affiliates. All rights reserved.
+#
+# This file is part of Navitia,
+#     the software to build cool stuff with public transport.
+#
+# Hope you'll enjoy and contribute to this project,
+#     powered by Canal TP (www.canaltp.fr).
+# Help us simplify mobility and open public transport:
+#     a non ending quest to the responsive locomotion way of traveling!
+#
+# LICENCE: This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+# Stay tuned using
+# twitter @navitia
+# IRC #navitia on freenode
+# https://groups.google.com/d/forum/navitia
+# www.navitia.io
+
+from flask import current_app
+import flask_restful
+import requests
+import logging
+
+class Validator(object):
+    def __init__(self):
+        self.jormungandr_url = current_app.config.get('JORMUNGANDR_URL')
+        self.jormungandr_token = current_app.config.get('JORMUNGANDR_TOKEN')
+        self.coverage = current_app.config.get('COVERAGE')
+        self._collections = {"stop_point": "stop_points",
+                             "VehicleJourney": "vehicle_journeys",
+                             "line": "lines",
+                             "network": "networks",
+                             "stop_area": "stop_areas"}
+
+    def is_object_valide(self, uri, object_type):
+        if object_type in self._collections.keys():
+            collection = self._collections[object_type]
+            url = "%s/v1/coverage/%s/%s/%s" % (self.jormungandr_url, self.coverage, collection, uri)
+            if self.jormungandr_token:
+                auth = (self.jormungandr_token, None)
+            else:
+                auth = None
+            request_jormun = requests.get(url,
+                                          auth=auth)
+            logging.getLogger(__name__).debug("call %s" % request_jormun.url)
+            if request_jormun:
+                json = request_jormun.json()
+                if collection in json and len(json[collection]) > 0:
+                    return True
+                return False
+            logging.getLogger(__name__).debug("url %s is not accessible" % request_jormun.url)
+            return False
