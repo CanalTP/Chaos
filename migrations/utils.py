3a65cee45100d9ec4a56b2bfba37a51a0a9c12ab
Fahmi Laajimi <fahmi.laajimi@canaltp.fr>
2019-10-02 15:39:00 +0200

    Fix identtation

--- migrations/utils.py
+++ migrations/utils.py
@@ -26,11 +26,10 @@ def column_exists(table_name, column_name):
     connection = op.get_bind()
     result = connection.execute(
         "SELECT EXISTS ( \
-         SELECT 1  \
-        FROM information_schema.columns \
-        WHERE table_schema='public' \
-        AND table_name='{}' \
-        AND column_name='{}') as ix_exists;".format(table_name, column_name)
-            .format(table_name)
+            SELECT 1  \
+            FROM information_schema.columns \
+            WHERE table_schema='public' \
+            AND table_name='{}' \
+            AND column_name='{}') as ix_exists;".format(table_name, column_name).format(table_name)
     ).first()
     return result.ix_exists
